457286c7d518ea1e1b69166fb62949443305436c
wideputin117 <takermanish7@gmail.com>
2025-09-09 20:48:44 +0530

    added the port and rest of things

--- main.go
+++ main.go
@@ -5,28 +5,17 @@ import (
 	"example/go-gin-resume-sender/routes"
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
-}
-
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
-
-// func for getting all the albums
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
-}
 func main() {
+	port := os.Getenv("PORT")
+	fmt.Print(port)
+	if port == "" {
+    port = "5000" // default fallback for local dev
+}
 	db := config.ConnectToDB()
 	collection := db.Database("Gin").Collection("user")
 	fmt.Print("the collection", collection)
@@ -37,11 +26,10 @@ func main() {
 			"message": "pong",
 		})
 	})
-	router.GET("/albums", getAlbums)
 	routes.AuthRoutes(router)
 	routes.CategoryRoutes(router)
 	routes.ProductRoutes(router)
 	routes.CompanyRoutes(router)
 	routes.ResumeRoutes(router)
-	router.Run("localhost:8000")
+	router.Run(":" + port)
 }
